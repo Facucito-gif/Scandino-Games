d3324acfd6e60450b5fbcb80bb92dc4bf1d937ff
ninetycode <mattez19@live.com>
2026-03-10 23:21:28 -0300

    MAIN MENU .01
    movi un archivo

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Assets/icon.tscn")
+	get_tree().change_scene_to_file("res://Scenes/icon.tscn")
 
 func _on_button_2_pressed() -> void:
 	pass
